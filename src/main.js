7717600d8576e823ca1348602201e5722deb86ac
Pun777 <lllpppp22@email.com>
2025-05-28 15:15:32 +0700

    first commit

--- src/main.js
+++
@@ -1,8 +0,0 @@
-// src/main// main.js
-// main.js
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './style.css'
-
-createApp(App).use(router).mount('#app')
